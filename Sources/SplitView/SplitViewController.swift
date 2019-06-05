ca6958eb0c4a33e34a5c230d754a1be92c531eb1
Alexsander Akers <me@a2.io>
2019-06-05 10:24:40 +0200

    Make structs public

--- Sources/SplitView/SplitViewController.swift
+++ Sources/SplitView/SplitViewController.swift
@@ -1,17 +1,17 @@
 import SwiftUI
 
-struct SplitViewController: UIViewControllerRepresentable {
-    var controllers: [UIViewController]
-    @Binding var preferredDisplayMode: UISplitViewController.DisplayMode
+public struct SplitViewController: UIViewControllerRepresentable {
+    public var controllers: [UIViewController]
+    @Binding public var preferredDisplayMode: UISplitViewController.DisplayMode
 
-    func makeUIViewController(context: UIViewControllerRepresentableContext<SplitViewController>) -> UISplitViewController {
+    public func makeUIViewController(context: UIViewControllerRepresentableContext<SplitViewController>) -> UISplitViewController {
         let splitViewController = UISplitViewController()
         splitViewController.preferredDisplayMode = preferredDisplayMode
         splitViewController.viewControllers = controllers
         return splitViewController
     }
 
-    func updateUIViewController(_ uiViewController: UISplitViewController, context: UIViewControllerRepresentableContext<SplitViewController>) {
+    public func updateUIViewController(_ uiViewController: UISplitViewController, context: UIViewControllerRepresentableContext<SplitViewController>) {
         uiViewController.preferredDisplayMode = preferredDisplayMode
         uiViewController.viewControllers = controllers
     }
